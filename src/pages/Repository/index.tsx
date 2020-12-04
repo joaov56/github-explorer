4752ff6f6ad06605ad07a5043558968ee31c8f3e
João Vitor <joao@MacBook-Pro-de-Mac-2.local>
2020-12-04 10:24:16 -0300

    add repository details style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-interface RepositoryParams{
-  repository: string;
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-}
+const Repository: React.FC = () => (
+  <>
+    <Header>
+      <img src={logoImg} alt="Github Explorer" />
+      <Link to="/">
+        <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+    </Header>
 
-const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>();
+    <RepositoryInfo>
+      <header>
+        <img src="https://avatars2.githubusercontent.com/u/61507139?v=4" alt="profile" />
+        <div>
+          <strong>joaov56/happy-rocketseat</strong>
+          <p>descrição do repositório</p>
+        </div>
+      </header>
+      <ul>
+        <li>
+          <strong>1808</strong>
+          <span>Stars</span>
+        </li>
+        <li>
+          <strong>48</strong>
+          <span>Forks</span>
+        </li>
+        <li>
+          <strong>67</strong>
+          <span>Issues abertas</span>
+        </li>
+      </ul>
+    </RepositoryInfo>
 
-  return (
-    <h1>
-      Repository
-      {params.repository}
-    </h1>
-  );
-};
+    <Issues>
+      <Link to="ok">
+        <div>
+          <strong>as</strong>
+          <p>as</p>
+        </div>
+        <FiChevronRight size={20} />
+      </Link>
+    </Issues>
+  </>
+);
 
 export default Repository;
